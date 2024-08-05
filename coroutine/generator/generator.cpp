e54faafc4d75b059e04d1cb879539c5c7514105e
luotf89 <1293971297@qq.com>
2024-08-05 19:03:11 +0800

    generator raw demo

--- coroutine/generator/generator.cpp
+++ coroutine/generator/generator.cpp
@@ -1,7 +1,43 @@
+#include <coroutine>
 #include <iostream>
-#include "generator.h"
+
+struct Generator {
+  struct promise_type {
+    std::suspend_never initial_suspend() { return {}; }
+    std::suspend_never final_suspend() noexcept { return {}; }
+    void unhandled_exception() {}
+    Generator get_return_object() {
+      return Generator{std::coroutine_handle<promise_type>::from_promise(*this)};
+    }
+
+    std::suspend_always await_transform(int value) {
+      this->value = value;
+      return {};
+    }
+
+    int value;
+  };
+
+  std::coroutine_handle<promise_type> handle;
+
+  int next() {
+    handle.resume();
+    return handle.promise().value;
+  }
+};
+
+Generator sequence() noexcept {
+  int i = 0;
+  while(true) {
+    co_await i++;
+  }
+}
 
 int main() {
-    std::cout << "hello generator!" << std::endl;
-    return 0;
+
+  Generator generator = sequence();
+  for (int i = 0; i < 10 ; i++) {
+    std::cout << generator.next() << std::endl;
+  }
+  return 0;
 }
