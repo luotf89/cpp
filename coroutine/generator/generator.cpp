74dad4e91e41aa36307441dcf9d66a859205bf12
luotf89 <1293971297@qq.com>
2024-08-07 15:03:52 +0800

    generator is fininsh

--- coroutine/generator/generator.cpp
+++ coroutine/generator/generator.cpp
@@ -1,10 +1,11 @@
 #include <coroutine>
 #include <iostream>
+#include <stdexcept>
 
 struct Generator {
   struct promise_type {
     std::suspend_never initial_suspend() { return {}; }
-    std::suspend_never final_suspend() noexcept { return {}; }
+    std::suspend_always final_suspend() noexcept { return {}; }
     void unhandled_exception() {}
     Generator get_return_object() {
       return Generator{std::coroutine_handle<promise_type>::from_promise(*this)};
@@ -12,24 +13,58 @@ struct Generator {
 
     std::suspend_always await_transform(int value) {
       this->value = value;
+      this->is_ready = true;
       return {};
     }
 
-    int value;
+    std::suspend_always yield_value(int value) {
+      this->value = value;
+      this->is_ready = true;
+      return {};
+    }
+
+    int value = -1;
+    bool is_ready = false;
   };
 
   std::coroutine_handle<promise_type> handle;
 
+  bool has_next() {
+    if (!handle || handle.done()) {
+      return false;
+    }
+    if (!handle.promise().is_ready) {
+      handle.resume();
+    }
+    if (handle.done()) {
+      return false;
+    }
+    return true;
+  }
+
   int next() {
-    handle.resume();
-    return handle.promise().value;
+    if (has_next()) {
+      handle.promise().is_ready = false;
+      return handle.promise().value;
+    }
+    return 555;
+    throw std::runtime_error("there is no valid value");
+  }
+
+  ~Generator() {
+    if (handle) {
+      handle.destroy();
+    }
   }
 };
 
 Generator sequence() noexcept {
   int i = 0;
-  while(true) {
+  while(i < 5) {
     co_await i++;
+  }
+  while(i < 10) {
+    co_yield i++;
   }
 }
 
@@ -37,7 +72,11 @@ int main() {
 
   Generator generator = sequence();
   for (int i = 0; i < 10 ; i++) {
-    std::cout << generator.next() << std::endl;
+    if (generator.has_next()) {
+      std::cout << "index: " << i << " value: " <<  generator.next() << std::endl;
+    } else {
+      break;
+    }
   }
   return 0;
 }
