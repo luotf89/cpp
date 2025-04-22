c85d36f7227982b7a1c142e06cbdb29e216a2d99
afei <1293971297@qq.com>
2025-04-22 10:10:28 +0800

    map instead of set to keep right order for visualization

--- algorithm/tree/radixtree/radixtree.cpp
+++ algorithm/tree/radixtree/radixtree.cpp
@@ -1,15 +1,17 @@
 #include "radixtree.h"
 #include <cassert>
 #include <random>
-#include <vector>
+#include <string>
 
 using namespace algorithm;
 
 std::pair<std::string, int> generator_prefix(unsigned long seed) {
     std::random_device rd;
     std::mt19937 generator(seed + rd());
-    std::uniform_int_distribution<int> distribution0(1, 1000);
+    // std::mt19937 generator(seed);
+    std::uniform_int_distribution<int> distribution0(1, 100);
     std::uniform_int_distribution<int> distribution1(0, 25);
+    // std::uniform_int_distribution<int> distribution1(0, 5);
     int len = distribution0(generator);
     std::string prefix;
     for(int i = 0; i < len; i++) {
@@ -20,21 +22,21 @@ std::pair<std::string, int> generator_prefix(unsigned long seed) {
 
 void test() {
     RadixTree<int> radixtree;
-    std::vector<std::string> prefixes;
-    int num = 10000;
+    std::set<std::string> prefixes;
+    int num = 100000;
     unsigned long seed = 0;
     for (int i = 0; i < num;i++) {
         auto prefix = generator_prefix(seed+i);
-        prefixes.emplace_back(prefix.first);
+        prefixes.insert(prefix.first);
         radixtree.insert(prefix.first, prefix.second);
-        assert(radixtree.isValid());  
+        assert(radixtree.isValid());
     }
-    // radixtree.visualization("graph.dot");
-    for (int i = 0; i < num; i++) {
-        assert(radixtree.search(prefixes[i]));
-        // radixtree.remove(prefixes[i]);
-        // assert(radixtree.isValid());
+    for (auto prefix: prefixes) {
+        assert(radixtree.search(prefix));
+        radixtree.remove(prefix);
+        assert(radixtree.isValid());
     }
+    radixtree.visualization("graph1.dot");
 }
 
 int main() {
--- algorithm/tree/radixtree/radixtree.h
+++ algorithm/tree/radixtree/radixtree.h
@@ -2,10 +2,12 @@
 
 #include <cassert>
 #include <set>
-#include<memory>
-#include<string>
-#include<sstream>
-#include<fstream>
+#include <map>
+#include <memory>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <iostream>
 
 /*
 有效信息挂在叶子节点 的set里面
@@ -17,12 +19,17 @@ template<typename T>
 struct RadixTreeNode {
   explicit RadixTreeNode(
     const std::string& prefix = "",
-    bool is_end = false): prefix_(prefix), is_end_(is_end) {}
-  std::set<std::shared_ptr<RadixTreeNode>> children_;
+    bool is_end = false): prefix_(prefix), is_end_(is_end), idx_(g_idx++) {}
+  std::map<char, std::shared_ptr<RadixTreeNode>> children_;
   std::string prefix_;
   std::set<T> metas_;
+  int idx_;
   bool is_end_;
+  static int g_idx;
 };
+
+template<typename T>
+int RadixTreeNode<T>::g_idx = 0;
 
 /*
 为了避免删除操作删除root节点，导致频繁调整root节点，将root 节点作为一个dummy节点
@@ -98,8 +105,8 @@ bool RadixTree<T>::insertImpl(
     T meta,
     std::shared_ptr<RadixTreeNode<T>> node) {
   // 因为采用 shared_ptr 可以不采用引用
-  for (auto child: node->children_) {
-    assert(child->prefix_.size() > 0);
+  if (node->children_.count(str[0]) != 0) {
+    auto child = node->children_.at(str[0]);
     int start  = 0;
     for (start = 0;
          start < child->prefix_.size() && start < str.size();
@@ -108,63 +115,62 @@ bool RadixTree<T>::insertImpl(
         break;
       }
     }
-    if (start > 0) {
-      if (start < child->prefix_.size()) {
-        // split child
-        auto new_node = std::make_shared<RadixTreeNode<T>>(
-          child->prefix_.substr(start), child->is_end_);
-        new_node->children_.swap(child->children_);
-        new_node->metas_.swap(child->metas_);
-        child->is_end_ = false;
-        child->prefix_ = child->prefix_.substr(0, start);
-        auto ret = child->children_.insert(new_node).second;
-        assert(ret);
-        if (start == str.size()) {
-          child->is_end_ = true;
-          child->metas_.insert(meta);
-        } else {
-          auto rest = std::make_shared<RadixTreeNode<T>>(str.substr(start), true);
-          rest->metas_.insert(meta);
-          child->children_.insert(rest);
-        }
-        return true;
+    if (start < child->prefix_.size()) {
+      // split child
+      auto new_node = std::make_shared<RadixTreeNode<T>>(
+        child->prefix_.substr(start), child->is_end_);
+      new_node->children_ = child->children_;
+      new_node->metas_ = child->metas_;
+      child->children_.clear();
+      child->metas_.clear();
+      child->is_end_ = false;
+      child->prefix_ = child->prefix_.substr(0, start);
+      auto ret = child->children_.insert({new_node->prefix_[0], new_node}).second;
+      assert(ret);
+      if (start == str.size()) {
+        child->is_end_ = true;
+        child->metas_.insert(meta);
       } else {
-        if (start == str.size()) {
-          child->is_end_ = true;
-          return child->metas_.insert(meta).second;
-        } else {
-          return insertImpl(str.substr(start), meta, child);
-        }
+        auto rest = std::make_shared<RadixTreeNode<T>>(str.substr(start), true);
+        rest->metas_.insert(meta);
+        child->children_.insert({rest->prefix_[0], rest});
+      }
+      return true;
+    } else {
+      if (start == str.size()) {
+        child->is_end_ = true;
+        return child->metas_.insert(meta).second;
+      } else {
+        return insertImpl(str.substr(start), meta, child);
       }
     }
+  } else {
+    auto new_node = std::make_shared<RadixTreeNode<T>>(str, true);
+    new_node->metas_.insert(meta);
+    return node->children_.insert({new_node->prefix_[0], new_node}).second;
   }
-  auto new_node = std::make_shared<RadixTreeNode<T>>(str, true);
-  new_node->metas_.insert(meta);
-  return node->children_.insert(new_node).second;
 }
 
 template<typename T>
 std::shared_ptr<RadixTreeNode<T>> RadixTree<T>::searchImpl(
     const std::string& str,
     std::shared_ptr<RadixTreeNode<T>> node) {
-  for (auto child: node->children_) {
-    assert(child->prefix_.size() > 0);
+  if (node->children_.count(str[0]) > 0) {
+    auto child = node->children_.at(str[0]);
     int start  = 0;
     for (start = 0; start < child->prefix_.size() && start < str.size(); start++) {
       if (str[start] != child->prefix_[start]) {
         break;
       }
-    } 
-    if (start > 0) {
-      if (start == child->prefix_.size()) {
-        if (start == str.size()) {
-          return child->is_end_? child: nullptr;
-        } else {
-          return searchImpl(str.substr(start), child);
-        }
+    }
+    if (start == child->prefix_.size()) {
+      if (start == str.size()) {
+        return child->is_end_? child: nullptr;
       } else {
-        return nullptr;
+        return searchImpl(str.substr(start), child);
       }
+    } else {
+      return nullptr;
     }
   }
   return nullptr;
@@ -174,49 +180,47 @@ template <typename T>
 bool RadixTree<T>::removeImpl(
     const std::string& str,
     std::shared_ptr<RadixTreeNode<T>> node) {
-  for(auto child: node->children_) {
-    assert(child->prefix_.size() > 0);
+  if (node->children_.count(str[0]) > 0) {
+    auto child = node->children_.at(str[0]);
     int start  = 0;
     for (start = 0; start < child->prefix_.size() && start < str.size(); start++) {
       if (str[start] != child->prefix_[start]) {
         break;
       }
-    } 
-    if (start > 0) {
-      if (start == child->prefix_.size()) {
-        if (start == str.size()) {
-          if (child->is_end_) {
-            child->is_end_ = false;
-            if (child->children_.empty() || child->children_.size() == 1) {
-              if (child->children_.empty()) {
-                // 如果child 节点的子节点为空这直接删除child节点
-                node->children_.erase(child);
-              } else {
-                // 如果child 节点的子节点为只有一个这压缩child 子节点到child节点
-                auto grandson = *child->children_.begin();
-                child->prefix_.append(grandson->prefix_);
-                child->children_ = grandson->children_;
-                child->metas_ = grandson->metas_;
-                child->is_end_ = grandson->is_end_;
-              }
-            }
-            // 如果该child被删除后 导致node节点只有一个子节点，者压缩child节点到子节点
-            // 注意当前采用sub_node 节点而不采用child节点，因为可能child节点被删除了。
-            if (node->children_.size() == 1 && !node->is_end_ && node != root_) {
-              auto sub_node = *node->children_.begin();
-              node->prefix_.append(sub_node->prefix_);
-              node->is_end_ = sub_node->is_end_;
-              node->metas_ = sub_node->metas_;
-              node->children_ = sub_node->children_;
-            }
-            return true;
+    }
+    if (start == child->prefix_.size()) {
+      if (start == str.size()) {
+        if (child->is_end_) {
+          child->is_end_ = false;
+          child->metas_.clear();
+          // 1、如果child 节点的子节点为空这直接删除child节点
+          if (child->children_.empty()) {
+            node->children_.erase(str[0]);
           }
-          return false;
-        } else {
-          return removeImpl(str.substr(start), child);
+          // 2、如果child节点的子节点为只有一个 者压缩child 子节点到child节点
+          // 注意这部分判断修改了了child->children 不能和 1 互换 
+          if (child->children_.size() == 1) {
+            auto grandson = child->children_.begin()->second;
+            child->prefix_.append(grandson->prefix_);
+            child->children_ = grandson->children_;
+            child->metas_ = grandson->metas_;
+            child->is_end_ = grandson->is_end_;
+          }
+          // 3、如果该child被删除后 导致node节点只有一个子节点，者压缩child节点到子节点
+          // 注意当前采用sub_node 节点而不采用child节点，因为可能child节点被删除了。
+          if (node->children_.size() == 1 && !node->is_end_ && node != root_) {
+            auto sub_node = node->children_.begin()->second;
+            node->prefix_.append(sub_node->prefix_);
+            node->is_end_ = sub_node->is_end_;
+            node->metas_ = sub_node->metas_;
+            node->children_ = sub_node->children_;
+          }
+          return true;
         }
-      } else {
+        // 如果child 节点不是叶子节点 者返回false
         return false;
+      } else {
+        return removeImpl(str.substr(start), child);
       }
     }
   }
@@ -232,16 +236,16 @@ void RadixTree<T>::visualizationImpl(std::ostringstream& oss, int ident, std::sh
     for (auto meta : node->metas_) {
       node_metas += std::to_string(meta);
     }
-    for (auto meta : child->metas_) {
+    for (auto meta : child.second->metas_) {
       child_metas += std::to_string(meta);
     }
     oss << std::string(ident, ' ');
-    oss << "\"" << node->prefix_ << "_" << (node->is_end_ ? "true": "false") << "_metas_" << node_metas << "\""
+    oss << "\"" << node->prefix_ << "_" << (node->is_end_ ? "true": "false") << "_" << node->idx_ << "_" <<"_metas_" << node_metas << "\""
         << " -> "
-        << "\""<< child->prefix_ << "_" << (child->is_end_ ? "true": "false") << "_metas_" << child_metas << "\""
+        << "\""<< child.second->prefix_ << "_" << (child.second->is_end_ ? "true": "false") << "_" << child.second->idx_ << "_" << "_metas_" << child_metas << "\""
         << "\n";
-    if (!child->children_.empty()) {
-      visualizationImpl(oss, ident+4, child);
+    if (!child.second->children_.empty()) {
+      visualizationImpl(oss, ident+4, child.second);
     }
   }
 }
@@ -249,22 +253,32 @@ void RadixTree<T>::visualizationImpl(std::ostringstream& oss, int ident, std::sh
 template<typename T>
 bool RadixTree<T>::isValidImpl(std::shared_ptr<RadixTreeNode<T>> node) {
   if (node != root_ && node->prefix_.empty()) {
+    std::cout << "node prefix is empty\n";
     return false;
   }
   if (node->children_.empty()) {
     return true;
   } else if (node->children_.size() == 1) {
     if (node != root_ && ! node->is_end_) {
+      std::cout << "node is not end and children num is one\n";
       return false;
     }
   }
+  if (!node->is_end_ && !node->metas_.empty()) {
+    std::cout << "node is not end but not empty\n";
+    return false;
+  }
+  if (node->is_end_ && node->metas_.empty()) {
+    std::cout << "node is end but empty\n";
+    return false;
+  }
   std::set<char> first_prefix_set;
   for (auto child : node->children_) {
-    if (first_prefix_set.count(child->prefix_[0]) > 0) {
+    if (first_prefix_set.count(child.second->prefix_[0]) > 0) {
       return false;
     }
-    first_prefix_set.insert(child->prefix_[0]);
-    if (!isValidImpl(child)) {
+    first_prefix_set.insert(child.second->prefix_[0]);
+    if (!isValidImpl(child.second)) {
       return false;
     }
   }
